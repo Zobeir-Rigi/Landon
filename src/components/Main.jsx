9d92b609568d75b3dc327e987e4c48aa637873e6
zobeir-rigi <rigi.zobir@yahoo.com>
2023-12-10 23:24:55 +0000

    separeted components

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import { Hotelinfo } from "./Hotelinfo"
+import { Welcome } from "./Welcome"
+
+export const Main = () => {
+    return (
+        <main id="wrapper">
+            <Welcome />
+            <Hotelinfo />
+      </main>
+    )
+}
