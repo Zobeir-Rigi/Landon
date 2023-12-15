eb9a5cb7ae136dccdb4e0c4421d68fc5e14cc181
zobeir-rigi <rigi.zobir@yahoo.com>
2023-12-15 15:02:17 +0000

    services added by json

--- src/components/Hotelinfo.jsx
+++ src/components/Hotelinfo.jsx
@@ -1,3 +1,5 @@
+import servicesData from './data/services.json'
+
 export const Hotelinfo = () =>{
   return (
    <div className="scene" id="hotelinfo">
@@ -20,21 +22,11 @@ export const Hotelinfo = () =>{
           <h2>Services and Amenities</h2>
           <p>Our services and amenities are designed to make your travel easy, your stay comfortable, and your experience one-of-a-kind.</p>
           <ul>
-            <li>Indoor pool</li>
-            <li>24-hour fitness center</li>
-            <li>Massage therapy</li>
-            <li>Full service spa</li>
-            <li>In-room jacuzzi tubs</li>
-            <li>Rooftop café  &amp; smoothie bar</li>
-            <li>Coffee bar  &amp; pastry shop</li>
-            <li>Traditional continental breakfast</li>
-            <li>24-hour concierge service</li>
-            <li>Business center</li>
-            <li>Complimentary wireless service</li>
-            <li>Laundry &amp; dry cleaning service</li>
-            <li>Daily paper</li>
-            <li>Certified "green" hotel</li>
-            <li>Pet-friendly rooms  &amp; common areas</li>
+            {
+              servicesData.map((service)=>  
+                <li>{service.name}</li>
+              )
+            }
           </ul>
         </section>
         <section className="checklist" id="accessibility">
