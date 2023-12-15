3e683a674d21378dbfcf99a5396f4c28d3b493ce
zobeir-rigi <rigi.zobir@yahoo.com>
2023-12-15 15:07:17 +0000

    accessibility ul created by json

--- src/components/Hotelinfo.jsx
+++ src/components/Hotelinfo.jsx
@@ -1,4 +1,5 @@
 import servicesData from './data/services.json'
+import accessibilityData from './data/accessibilities.json'
 
 export const Hotelinfo = () =>{
   return (
@@ -33,19 +34,11 @@ export const Hotelinfo = () =>{
           <h2>Accessibility</h2>
           <p>We're committed to maintaining the same quality of service for every individual. We offer the following facilities for those with special needs:</p>
           <ul>
-            <li>Grab bars on tub walls</li>
-            <li>Shower chairs</li>
-            <li>Hand held shower sprayers</li>
-            <li>Higher toilets &amp; toilet modifiers</li>
-            <li>Lower sink faucet handles</li>
-            <li>Wheelchair clearance under sinks &amp; vanity</li>
-            <li>Lower racks in closet</li>
-            <li>TDD machines</li>
-            <li>Telephone light signalers  &amp; smoke alarms</li>
-            <li>Telephone amplification handsets</li>
-            <li>Closed captioned television converters</li>
-            <li>Vibrating alarm clocks</li>
-            <li>Telephones with volume control</li>
+          {
+              accessibilityData.map((accessibility)=>  
+                <li>{accessibility.name}</li>
+              )
+            }
           </ul>
         </section>
       </article>
