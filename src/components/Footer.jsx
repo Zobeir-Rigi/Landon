bf2233a625029f5c7325512307d493b4b2191ca8
zobeir-rigi <rigi.zobir@yahoo.com>
2023-12-18 03:09:15 +0000

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ export const Footer = ()=> {
           <article className="content">
             <div id="socialmedia">
               <ul className="group">
-                <li><a href="https://twitter.com"><img className="icon" src="https://landonhotel.com/images/socialmedia/twitter.png" alt="icon for twitter" /></a></li>
-                <li><a href="http://www.facebook.com"><img className="icon" src="https://landonhotel.com/images/socialmedia/facebook.png" alt="icon for facebook" /></a></li>
-                <li><a href="http://www.youtube.com"><img className="icon" src="https://landonhotel.com/images/socialmedia/youtube.png" alt="icon for youtube" /></a></li>
+                <li><a href="https://github.com/Zobeir-Rigi" target="_blank"><img className="icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFDTKV4IUyFOpFh5_we4BJxAbFl9GaHYL5SRLfovXmuG0DpGXUPglO6d7CQwCE0X4tDRA&usqp=CAU" alt="icon for Github" /></a></li>
+                <li><a href="https://www.linkedin.com/in/zobeir-r-37068217b/ " target="_blank"><img className="icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj2LJ_EAM-FAO0gJnDwEHnB29Y2pGwqo2eeye7bcHu_LEgd-IHyvy9SHZJWdC0hDh_n-g&usqp=CAU" alt="icon for Linkedin" /></a></li>
+                <li><a href="https://www.codewars.com/users/Zobeir-Rigi" target="_blank"><img className="icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiKEm0ikEM0p9gR_SVUFOO3Upvc3i_okCAaHfXUtrYjeb6shYvFd6rpVjRncZe7tbv4LQ&usqp=CAU" alt="icon for Codewars" /></a></li>
               </ul>
             </div>
           </article>
